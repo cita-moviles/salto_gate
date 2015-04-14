fb76765c96ac9e4047946ce8bc4e9ca9695f1ce2
Carla Prieto <carlapchavez@gmail.com>
2015-04-13 19:14:21 -0600

    Initial commit

---
+++ touchid_demo/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  touchid_demo
+//
+//  Created by Admin on 4/9/15.
+//  Copyright (c) 2015 CITA. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+    @IBOutlet weak var lblAuthResult: UILabel!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func Authenticate_button(sender: UIButton) {
+        let authContext:LAContext = LAContext()
+        var error:NSError?
+        
+        //Is Touch ID hardware available & configured?
+        if(authContext.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error:&error))
+        {
+            //Perform Touch ID auth
+            authContext.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: "Testing Touch ID", reply: {(wasSuccessful:Bool, error:NSError?) in
+                
+                if(wasSuccessful)
+                {
+                    //User authenticated
+                    self.writeOutAuthResult(error)
+                }
+                else
+                {
+                    //There are a few reasons why it can fail, we'll write them out to the user in the label
+                    self.writeOutAuthResult(error)
+                }
+                
+            })
+            
+        }
+        else
+        {
+            //Missing the hardware or Touch ID isn't configured
+            self.writeOutAuthResult(error)
+        }
+        
+    }
+    
+    func writeOutAuthResult(authError:NSError?)
+    {
+        dispatch_async(dispatch_get_main_queue(), {() in
+            if let possibleError = authError
+            {
+                self.lblAuthResult.textColor = UIColor.redColor()
+                self.lblAuthResult.text = possibleError.localizedDescription
+                
+            }
+            else
+            {
+                /*self.lblAuthResult.textColor = UIColor.greenColor()
+                self.lblAuthResult.text = "Authentication successful."*/
+                
+                //Present the next view controller
+                println("Authentication Successfull")
+                let auth_vc = self.storyboard?.instantiateViewControllerWithIdentifier("auth_sucess") as auth_sucess
+                self.navigationController?.pushViewController(auth_vc, animated: true)
+                
+               
+            }
+        })
+        
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ touchid_demo/auth_sucess.swift
@@ -0,0 +1,36 @@
+//
+//  auth_sucess.swift
+//  touchid_demo
+//
+//  Created by Admin on 4/13/15.
+//  Copyright (c) 2015 CITA. All rights reserved.
+//
+
+import UIKit
+
+class auth_sucess: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("Can you see it now? Welcome to Swift!")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
